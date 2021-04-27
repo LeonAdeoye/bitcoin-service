a777f4f7a8fe4967c69dc7f20e852b6d5900af30
LeonAdeoye <leon.adeoye@gmail.com>
2021-04-27 20:55:02 +0800

    Wallet construction

--- src/main/java/com/leon/services/WalletServiceImpl.java
+++ src/main/java/com/leon/services/WalletServiceImpl.java
@@ -1,10 +1,7 @@
 package com.leon.services;
 
-import org.bitcoinj.core.BlockChain;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.PeerGroup;
-import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.wallet.KeyChainGroup;
 import org.bitcoinj.wallet.Wallet;
 import org.slf4j.Logger;
@@ -12,15 +9,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.math.BigInteger;
+import java.util.List;
 
 @Service
 public class WalletServiceImpl implements WalletService
 {
     private static final Logger logger = LoggerFactory.getLogger(WalletServiceImpl.class);
 
-    private static WalletAppKit kit;
+    private static Wallet wallet;
 
     @Autowired
     ConfigurationService configurationService;
@@ -33,28 +30,32 @@ public class WalletServiceImpl implements WalletService
     public void initialise()
     {
         String filePrefix = "bitcoin-service";
-
         NetworkParameters networkParameters = ConfigurationServiceImpl.getNetworkParams();
-
-//        kit = new WalletAppKit(networkParameters, new File("."), filePrefix);
-//
-//        KeyChainGroup group = new KeyChainGroup(networkParameters, true);
-//        Wallet wallet = new Wallet(networkParameters, group);
+        KeyChainGroup group = KeyChainGroup.createBasic(networkParameters);
+        wallet = new Wallet(networkParameters, group);
     }
 
     public void addKey(String privateKeyAsHex)
     {
-        //wallet.importKey(new ECKey().fromPrivate(new BigInteger(privateKeyAsHex, 16)));
+        ECKey newKey = new ECKey().fromPrivate(new BigInteger(privateKeyAsHex, 16));
+        wallet.importKey(newKey);
+        logger.info("Imported key with public key: " + newKey.getPubKey());
     }
 
     public void addKey(ECKey key)
     {
-        //kit.wallet().importKey(key);
+        wallet.importKey(key);
+        logger.info("Imported key with public key: " + key.getPubKey());
+    }
+
+    public void addKeys(List<ECKey> keys)
+    {
+        wallet.importKeys(keys);
+        logger.info("Imported a list of " + keys.size() + " keys.");
     }
 
     public boolean hasKey(ECKey key)
     {
-        //return kit.wallet().hasKey(key);
-        return false;
+        return wallet.hasKey(key);
     }
 }
